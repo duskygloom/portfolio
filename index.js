2b99360fc43391abfa661a4cfdd5e21dfca61cc2
duskygloom <sarkarharsh708@gmail.com>
2024-09-14 14:06:59 +0530

    First commit.

---
+++ index.js
@@ -0,0 +1,13 @@
+function spinPicture() {
+    // let element = document.getElementById('yay-text');
+    // // element.hidden = !element.hidden;
+    // element = document.getElementById('my-picture');
+    // element.style = "transform: rotate(1440deg); transition-duration: 2000ms"
+}
+
+function mirrorPicture() {
+    // let element = document.getElementById('yay-text');
+    // // element.hidden = !element.hidden;
+    // element = document.getElementById('my-picture');
+    // element.style = "transform: rotate(1440deg); transition-duration: 2000ms"
+}
